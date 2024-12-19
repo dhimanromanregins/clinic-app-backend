121a9b8f1dd85c9d857ee6a3cf71702afeb033f5
Sahil dhiman <dhimansahil.ameotech@gmail.com>
2024-12-19 16:29:21 +0530

    updates

--- accounts/models.py
+++ accounts/models.py
@@ -84,6 +84,8 @@ class Notifications(models.Model):
     title = models.CharField(max_length=255)
     body = models.TextField()
     is_read = models.BooleanField(default=False)
+    created_at = models.DateTimeField(auto_now_add=True, null=True, blank=True)
+    updated_at = models.DateTimeField(auto_now=True, null=True, blank=True)
 
     def __str__(self):
         return f"Notification for {self.user}"
